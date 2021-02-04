181b4a19130a68269f29a64c3e6fcf077381f2a9
younhong <younhong@kakao.com>
2021-02-04 22:37:08 +0900

    update home

--- screens/Home.js
+++ screens/Home.js
@@ -329,6 +329,14 @@ const Home = () => {
     const [restaurants, setRestaurants] = React.useState(restaurantData)
     const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
 
+    function onSelectCategory(category) {
+        let restaurantList = restaurantData.filter(
+            a => a.categories.includes(category.id)
+        );
+        setRestaurants(restaurantList);
+        setSelectedCategory(category);
+    };
+
     function renderHeader() {
         return (
             <View style={{flexDirection: 'row', height: 50}}>
@@ -362,7 +370,7 @@ const Home = () => {
                         }}
                     >
                         <Text style={{...FONTS.h3}}>
-                            Location
+                            {currentLocation.streetName}
                         </Text>
                     </View>
                 </View>
@@ -385,11 +393,136 @@ const Home = () => {
         )
     };
 
+    function renderMainCategories() {
+        const renderItem = ({item}) => {
+            return (
+                <TouchableOpacity 
+                    style={{
+                        padding: SIZES.padding,
+                        paddingBottom: SIZES.padding * 2,
+                        backgroundColor: (selectedCategory?.id == item.id)
+                            ? COLORS.primary : COLORS.white,
+                        borderRadius: SIZES.radius,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginRight: SIZES.padding,
+                        ...styles.shadow
+                    }}
+                    onPress={() => onSelectCategory(item)}
+                >
+                    <View style={{
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: (selectedCategory?.id == item.id) 
+                            ? COLORS.white : COLORS.lightGray
+                    }}>
+                        <Image 
+                            source={item.icon}
+                            resizeMode='contain'
+                            style={{
+                                width: 30,
+                                height: 30
+                            }}
+                        />
+                    </View>
+                    <Text
+                        style={{
+                            marginTop: SIZES.padding,
+                            color: (selectedCategory?.id == item.id) 
+                                ? COLORS.white : COLORS.black,
+                            ...FONTS.body5
+                        }}
+                    >
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
+            )
+        };
+
+        return (
+            <View style={{
+                padding: SIZES.padding * 2
+            }}>
+                <Text style={{...FONTS.h1}}>Main</Text>
+                <Text style={{...FONTS.h1}}>Categories</Text>
+
+                <FlatList 
+                    data={categories}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={renderItem}
+                    contentContainerStyle={{
+                        paddingVertical: SIZES.padding * 2
+                    }}
+                />
+            </View>
+        );
+    };
+
+    function renderRestaurantList() {
+        const renderItem = ({item}) => (
+            <TouchableOpacity 
+                style={{
+                    marginBottom: SIZES.padding * 2
+                }}
+            >
+                <View>
+                    <Image 
+                        source={item.photo}
+                        resizeMode="cover"
+                        style={{
+                            width: '100%',
+                            height: 200,
+                            borderRadius: SIZES.radius
+                        }}
+                    />
+
+                    <View style={{
+                        position: 'absolute',
+                        bottom: 0,
+                        height: 50,
+                        width: SIZES.width * 0.3,
+                        backgroundColor: COLORS.white,
+                        borderTopRightRadius: SIZES.radius,
+                        borderBottomLeftRadius: SIZES.radius,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        ...styles.shadow
+                    }}>
+                        <Text style={{...FONTS.h4}}>
+                            {item.duration}
+                        </Text>
+
+                    </View>
+
+                </View>
+
+            </TouchableOpacity>
+        );
+        return (
+            <FlatList
+                data={restaurants}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                contentContainerStyle={{
+                    paddingHorizontal: SIZES.padding * 2,
+                    paddingBottom: 30
+                }}
+            />
+        )
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             {renderHeader()}
+            {renderMainCategories()}
+            {renderRestaurantList()}
         </SafeAreaView>
-    )
+    );
 };
 
 const styles = StyleSheet.create({
@@ -407,6 +540,6 @@ const styles = StyleSheet.create({
         shadowRadius: 3,
         elevation: 1
     }
-})
+});
 
 export default Home;
